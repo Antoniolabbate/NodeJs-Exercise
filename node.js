beadd78955b5224e8c5a173a653c3b037f49ea93
antonio l'abbate <antolabb06@gmail.com>
2023-06-12 10:42:19 +0200

    exercise-8

--- node.js
+++ node.js
@@ -1,5 +1,39 @@
-const fs = require('fs');
-fs.writeFile("mynewfile3.txt", "Hello content !", function(err){
-    if(err) throw err;
-    console.log("saved");
-})
+function luckyDraw(player) {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(`${player} won a prize in the draw!`);
+        } else {
+          reject(new Error(`${player} lost the draw.`));
+        }
+      });
+    });
+  }
+
+  luckyDraw("Joe")
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+  .then(() => {
+    return luckyDraw("Caroline");
+  })
+  .then((result) => {
+    console.log(result)
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+  .then(() => {
+    return luckyDraw("Sabrina");
+  })
+  .then((result) => {
+    console.log(result)
+  })
+  .catch((error) => {
+    console.error(error);
+  })
